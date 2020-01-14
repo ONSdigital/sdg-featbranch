88f1fe5b44382d51b1f28100bec34a9b3fa83842
SavvasStephanides <savvascyp@hotmail.com>
2020-01-14 17:24:27 +0000

    Refactor data.buildbranch

--- containers/data.buildbranch/content/api.py
+++ containers/data.buildbranch/content/api.py
@@ -6,18 +6,32 @@ import shutil
 app = Flask(__name__)
 
 @app.route('/<branch_name>')
-def build_data_branch(branch_name):
-    subprocess.call(["sh", "/api/build.sh"], env={"branch": branch_name})
-    build_successful = os.path.exists("/tmp/data/_site")
+def execute(branch_name):
+    build_branch(branch_name)
+    built_successfully = build_completed_successfully()
 
-    if build_successful:
-        if os.path.exists("/server/data/" + branch_name):
-            shutil.rmtree("/server/data/" + branch_name)
-            
-        shutil.copytree("/tmp/data/_site", "/server/data/" + branch_name)
+    if built_successfully:
+        remove_existing_directory_from_server(branch_name)
+        copy_built_site_to_server(branch_name)
 
-    shutil.rmtree("/tmp/data")
+    clean_up()
         
     return {
-        "builtSuccessfully": build_successful
+        "builtSuccessfully": built_successfully
     }
+
+def build_branch(branch_name):
+    subprocess.call(["sh", "/api/build.sh"], env={"branch": branch_name})
+
+def build_completed_successfully():
+    return os.path.exists("/tmp/data/_site")
+
+def remove_existing_directory_from_server(branch_name):
+    if os.path.exists("/server/data/" + branch_name):
+        shutil.rmtree("/server/data/" + branch_name)
+
+def copy_built_site_to_server(branch_name):
+    shutil.copytree("/tmp/data/_site", "/server/data/" + branch_name)
+
+def clean_up():
+    shutil.rmtree("/tmp/data")

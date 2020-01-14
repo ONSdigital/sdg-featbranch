d9e9f7c86aaf71275e49f6efecb32e8aa757c5f7
SavvasStephanides <savvascyp@hotmail.com>
2020-01-14 16:50:14 +0000

    Refactor app.settings service

--- containers/app.settings/content/app.go
+++ containers/app.settings/content/app.go
@@ -2,20 +2,26 @@ package main
 
 import (
     "fmt"
-    "log"
 	"net/http"
 	"io/ioutil"
 )
 
 func main() {
-    http.HandleFunc("/", getSetting)
-    log.Fatal(http.ListenAndServe(":80", nil))
+    http.HandleFunc("/", handle)
+    http.ListenAndServe(":80", nil)
 }
 
-func getSetting(w http.ResponseWriter, r *http.Request){
-	name := r.URL.Path[1:]
+func handle(responseWriter http.ResponseWriter, request *http.Request){
+	settingName := getSettingNameFromRequest(request)
+	settingValue := getSettingValue(settingName)
+	fmt.Fprintf(responseWriter, "%s", string(settingValue))
+}
 
+func getSettingNameFromRequest(request *http.Request) string{
+	return request.URL.Path[1:]
+}
+
+func getSettingValue(name string) string{
 	value, _ := ioutil.ReadFile("/settings/" + name)
-
-	fmt.Fprintf(w, "%s", string(value))
+	return string(value)
 }
